da786e2cbc9f19a861fb2b7da592d9466a3be0c6
Darya <daryapogas@gmail.com>
2026-07-25 00:12:11 -0500

    feat(auth): connect user registration route

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
 
+const authRoutes = require('./routes/auth.routes');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 const app = express();
@@ -12,6 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 const limiter = rateLimit({
@@ -21,11 +25,14 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Routes
-
+app.use('/api/auth', authRoutes);
 
 // Root route
 app.get('/', (req, res) => {
   res.send('Backend API is running');
 });
+
+// Handle controller errors
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
