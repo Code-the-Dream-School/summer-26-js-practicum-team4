a994c6ea59c48b7cabf31992cafcfe969aee0213
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-07-28 10:45:28 -0700

    corrected controller file paths that were removed in prior merge, partially implemented getUserPatterns endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
 
 // Route Imports
-const helloRoutes = require("./routes/hello.routes");
 const userRoutes = require("./routes/user.routes");
 const patternRoutes = require("./routes/pattern.routes");
+const authRoutes = require("./routes/auth.routes");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const app = express();
 
@@ -17,7 +19,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -26,7 +28,9 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/patterns", patternRoutes);
+app.use("/api/users", userRoutes);
 
 // Root route
 app.get("/", (req, res) => {
--- backend/src/controllers/pattern.controller.js
+++ backend/src/controllers/pattern.controller.js
@@ -1,6 +1,22 @@
-function getAllUserPatterns(req, res) {
-  return res.send({
-    message: "Behold all of your patterns... yeah we can't do that yet",
+// Imports
+const { StatusCodes } = require("http-status-codes");
+const prisma = require("../config/prismaClient");
+
+async function getAllUserPatterns(req, res) {
+  // Assumes userId is stored in req.user
+  const userPatterns = await prisma.pattern.findMany({
+    where: { userId: 1 },
+    select: {
+      id: true,
+      patternName: true,
+      patternImgUrl: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return res.status(StatusCodes.OK).json({
+    patterns: userPatterns,
   });
 }
 
