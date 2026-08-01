3f20f92e575950e3a7ec7946fb9aa1b9e25104df
Oksana Mosendz <bagheera03@gmail.com>
2026-08-01 01:17:56 -0500

    add loader component

--- frontend/src/pages/GalleryPage.jsx
+++ frontend/src/pages/GalleryPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Loader from "../components/Loader/Loader";
 
 function GalleryPage() {
   return (
     <div>
       <h1>Gallery Page</h1>
+      <Loader />
     </div>
   );
 }
