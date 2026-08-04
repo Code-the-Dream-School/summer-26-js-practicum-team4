634287579d3bfc3d1eecc7a8d2b80a1dfc145a73
Jahaira Flores <jahaira@umich.edu>
2026-08-04 15:50:14 -0500

    Update auth forms to dispatch authentication state changes

--- frontend/src/components/features/auth/LoginForm.jsx
+++ frontend/src/components/features/auth/LoginForm.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { login } from "../../../services/authService";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../../state/auth/useAuth";
 
 function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const { dispatch } = useAuth();
 
     async function handleSubmit(e) {
         e.preventDefault();
  
         try {
-            const user = await login(email, password);
-            console.log("Logged in:", user);
+            const user = await login(email, password)
+            dispatch({
+                type: "LOGIN_SUCCESS",
+                payload: user,
+            });
         } catch(error) {
             console.error(error);
         }
--- frontend/src/components/features/auth/RegisterForm.jsx
+++ frontend/src/components/features/auth/RegisterForm.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import { register } from "../../../services/authService";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../../state/auth/useAuth";
 
 function RegisterForm() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+     const { dispatch } = useAuth();
+    
     async function handleSubmit(e) {
         e.preventDefault();
  
         try {
             const user = await register(username, email, password);
-            console.log("Registered:", user);
+            dispatch({
+                type: "LOGIN_SUCCESS",
+                payload: user,
+            });
         } catch(error) {
             console.error(error);
         }
