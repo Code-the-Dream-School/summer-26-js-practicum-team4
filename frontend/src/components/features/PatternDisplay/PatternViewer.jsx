ca5c15ea1da8c84a44ea4978a3cb5387b5bf97eb
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-08-05 03:20:47 -0700

    scaled test photos

--- frontend/src/components/features/PatternDisplay/PatternViewer.jsx
+++ frontend/src/components/features/PatternDisplay/PatternViewer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PatternViewer({ pattern }) {
   console.log(pattern);
-  return <img src={pattern.patternImgUrl} />;
+  return <img src={pattern.patternImgUrl} height={200} width={200} />;
 }
 
 export default PatternViewer;
