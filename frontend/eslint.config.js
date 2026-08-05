58abcc721f3285a08827ee2c052279e171cb09a3
Jahaira Flores <jahaira@umich.edu>
2026-08-05 13:51:33 -0500

    Fix ESLint Node globals configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -22,7 +22,10 @@ export default defineConfig([
     },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+       globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
