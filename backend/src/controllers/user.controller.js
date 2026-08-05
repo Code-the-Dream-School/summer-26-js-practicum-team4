46c182c50929b048809b98cbb361e179bb24197a
Oksana Mosendz <bagheera03@gmail.com>
2026-08-05 14:52:14 -0500

    add delete user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,17 @@
+const prisma = require("../config/prismaClient");
+const { StatusCodes } = require("http-status-codes");
+
 const getUser = (req, res) => {};
 const updateUser = (req, res) => {};
-const deleteUser = (req, res) => {};
+
+const deleteUser = async (req, res) => {
+  await prisma.user.delete({
+    where: { id: req.user.userId },
+  });
+  res.clearCookie("token");
+  return res.status(StatusCodes.OK).json({
+    message: "User deleted successfully.",
+  });
+};
 
 module.exports = { getUser, updateUser, deleteUser };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,14 +1,18 @@
 const express = require("express");
-
+const authenticationMiddleware = require("../middleware/authentication.js");
 // Route handler function imports
-const {} = require("../controllers/user.controller");
+const {
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 // Router to handle all routes to users
 const router = express.Router();
 
 // Routes
-router.get("/profile", getUser);
-router.patch("/profile", updateUser);
-router.delete("/profile", deleteUser);
+router.get("/profile", authenticationMiddleware, getUser);
+router.patch("/profile", authenticationMiddleware, updateUser);
+router.delete("/profile", authenticationMiddleware, deleteUser);
 
 module.exports = router;
