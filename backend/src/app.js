bc3cb5d8fbd91043deb4eb82153f9c729d550687
Oksana Mosendz <bagheera03@gmail.com>
2026-08-14 17:15:15 -0500

    add notFoundmiddleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const patternRoutes = require("./routes/pattern.routes");
 
 // Error Middleware Import
 const errorHandlerMiddleware = require("./middleware/error-handler");
-
+const notFoundMiddeleware = require("./middleware/notFound.middleware");
 const app = express();
 
 // Security & best‑practice middleware
@@ -55,6 +55,7 @@ app.use((error, req, res, next) => {
   return next(error);
 });
 
+app.use(notFoundMiddeleware);
 // Handle controller errors
 app.use(errorHandlerMiddleware);
 
