dde3646c555308449e6abbd89458e183af534caa
Oksana Mosendz <bagheera03@gmail.com>
2026-08-15 23:19:28 -0500

    make pattern parameters optional, add supported values  to maxColors

--- backend/src/validation/patternSchema.js
+++ backend/src/validation/patternSchema.js
@@ -1,23 +1,27 @@
 const Joi = require("joi");
 
 const patternParameters = {
-  stitchWidth: Joi.number().integer().min(10).max(200).messages({
+  stitchWidth: Joi.number().integer().min(10).max(200).optional().messages({
     "number.base": "Stitch width must be a number.",
     "number.integer": "Stitch width must be an integer.",
     "number.min": "Stitch width must be at least 10.",
     "number.max": "Stitch width must be no more than 200.",
   }),
-  stitchHeight: Joi.number().integer().min(10).max(200).messages({
+  stitchHeight: Joi.number().integer().min(10).max(200).optional().messages({
     "number.base": "Stitch height must be a number.",
     "number.integer": "Stitch height must be an integer.",
     "number.min": "Stitch height must be at least 10.",
     "number.max": "Stitch height must be no more than 200.",
   }),
-  maxColors: Joi.number().integer().min(1).messages({
-    "number.base": "Max colors must be a number.",
-    "number.integer": "Max colors must be an integer.",
-    "number.min": "Max colors must be at least 1.",
-  }),
+  maxColors: Joi.number()
+    .integer()
+    .valid(8, 16, 24, 32, 48)
+    .optional()
+    .messages({
+      "number.base": "Max colors must be a number.",
+      "number.integer": "Max colors must be an integer.",
+      "any.only": "Max colors must be 8, 16, 24, 32 or 48.",
+    }),
 };
 
 const patternSchema = Joi.object({
