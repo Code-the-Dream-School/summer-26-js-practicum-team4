8b9bf3b0cbe176ea3d8ae9f8c080e9da8aafd751
Oksana Mosendz <bagheera03@gmail.com>
2026-08-26 13:46:12 -0500

    install google-auth-library, add google token verification service

---
+++ backend/src/services/verifyGoogleIdToken.service.js
@@ -0,0 +1,26 @@
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const verifyGoogleIdToken = async (token) => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+
+    const {
+      sub: googleId,
+      email,
+      name: userName,
+      picture: userProfileImgUrl,
+    } = payload;
+
+    return { googleId, email, userName, userProfileImgUrl };
+  } catch (error){
+     throw new Error(`Google verification failed: ${error.message}`);
+  }
+};
+
+module.exports = { verifyGoogleIdToken };
+
