0a98a2317fc9a8d6d55b2eecdee5b87e2ef5ac42
Jahaira Flores <jahaira@umich.edu>
2026-08-29 20:47:59 -0500

    Fix padding in How It Works section

--- frontend/src/components/features/landing/HowItWorksSection.jsx
+++ frontend/src/components/features/landing/HowItWorksSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HowItWorksSection() {
   return (
-    <section className="relative z-10 mx-auto -mb-60 w-[90%] rounded-4xl bg-primary py-24 px-10 text-white">
+    <section className="relative z-10 mx-auto -mb-60 w-[90%] rounded-4xl bg-primary py-20 px-12 text-white">
       <h2 className="mb-16 text-center text-4xl font-bold">
         How does X-Stitch work?
       </h2>
@@ -12,7 +12,7 @@ function HowItWorksSection() {
           <img
             src="images/upload.png"
             alt="Step 1"
-            className="w-14 md:w-22 lg:w-24"
+            className="w-14 md:w-20 lg:w-20"
           />
           <h3 className="mt-6 text-3xl font-semibold">1. Upload</h3>
           <p className="mt-4 text-xl">
@@ -25,7 +25,7 @@ function HowItWorksSection() {
           <img
             src="images/edit.png"
             alt="Step 2"
-            className="w-14 md:w-22 lg:w-24"
+            className="w-14 md:w-20 lg:w-20"
           />
           <h3 className="mt-6 text-3xl font-semibold">2. Adjust</h3>
           <p className="mt-4 text-xl">
@@ -38,7 +38,7 @@ function HowItWorksSection() {
           <img
             src="images/grid.png"
             alt="Step 3"
-            className="w-14 md:w-22 lg:w-24"
+            className="w-14 md:w-20 lg:w-20"
           />
           <h3 className="mt-6 text-3xl font-semibold">3. Stitch</h3>
           <p className="mt-4 text-xl">
