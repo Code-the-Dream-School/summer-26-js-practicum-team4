4294d0d51af3b5b6c16fd1c6af675ca79ea6afcb
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-08-31 00:56:25 -0700

    created patternParameters in palette and grid, but because they both have to be arrays, there is now a helper function in pattern.controller.js that parses all JSON before passing it into the validator

--- backend/src/validation/patternSchema.js
+++ backend/src/validation/patternSchema.js
@@ -1,5 +1,42 @@
 const Joi = require("joi");
 
+const paletteSchema = Joi.object({
+  dmcCode: Joi.number().integer().required().messages({
+    "number.base": "dmcCode must be a number.",
+    "number.integer": "dmcCode must be an integer.",
+  }),
+  name: Joi.string().required().messages({
+    "string.base": "Name must be a string.",
+  }),
+  r: Joi.number().integer().min(0).max(255).required().messages({
+    "number.base": "r must be a number.",
+    "number.integer": "r must be an integer.",
+    "number.min": "r must be at least 0.",
+    "number.max": "r must be no more than 255.",
+  }),
+  g: Joi.number().integer().min(0).max(255).required().messages({
+    "number.base": "g must be a number.",
+    "number.integer": "g must be an integer.",
+    "number.min": "g must be at least 0.",
+    "number.max": "g must be no more than 255.",
+  }),
+  b: Joi.number().integer().min(0).max(255).required().messages({
+    "number.base": "b must be a number.",
+    "number.integer": "b must be an integer.",
+    "number.min": "b must be at least 0.",
+    "number.max": "b must be no more than 255.",
+  }),
+  symbol: Joi.string().trim().min(1).max(1).required().messages({
+    "string.min": "Symbol must be exactly 1 character.",
+    "string.max": "Symbol must be exactly 1 character.",
+  }),
+});
+
+const gridSchema = Joi.number().integer().messages({
+  "number.base": "Grid index must be a number",
+  "number.integer": "Grid index must be an integer",
+});
+
 const patternParameters = {
   stitchWidth: Joi.number().integer().min(10).max(200).required().messages({
     "number.base": "Stitch width must be a number.",
@@ -13,8 +50,8 @@ const patternParameters = {
     "number.min": "Stitch height must be at least 10.",
     "number.max": "Stitch height must be no more than 200.",
   }),
-  palette: {},
-  grid: {},
+  palette: Joi.array().items(paletteSchema),
+  grid: Joi.array().items(gridSchema),
 };
 
 const patternSchema = Joi.object({
