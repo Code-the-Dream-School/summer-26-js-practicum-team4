961eb9114f8f81a078b12ff51171f9b6bee5cad3
Jahaira Flores <jahaira@umich.edu>
2026-09-07 11:00:46 -0500

    update auth form styling

--- frontend/src/components/features/auth/LoginForm.jsx
+++ frontend/src/components/features/auth/LoginForm.jsx
@@ -77,7 +77,7 @@ function LoginForm() {
                   dispatch({ type: "CLEAR_ERROR" });
                 }}
                 required
-                className="w-full rounded-lg border border-gray-300 bg-[#F2F2F7] px-4 py-3 focus:border-gray-400 focus:outline-none"
+                className="w-full rounded-lg border border-gray-300 bg-input-form px-4 py-3 focus:border-gray-400 focus:outline-none"
               />
             </div>
 
